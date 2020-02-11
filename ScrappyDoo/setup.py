04e119c8b03fe0e57bd0978da3da66ccb6af5449
Ben Knight <benknight13@gmail.com>
2020-02-11 08:48:49 +0000

    update version number

--- ScrappyDoo/setup.py
+++ ScrappyDoo/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ScrappyDoo",
-    version="0.0.1.4",
+    version="0.0.1.5",
     author="Ben Knight",
     author_email="info@bknight.co.uk",
     description="Scrape data from website",
